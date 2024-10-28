65b2dfaef7dd0a6e739dd2dd642b595ef1d1c18d
ChelsMarie <vchelseamariev@gmail.com>
2024-10-28 00:20:38 -0400

    Adding logo again

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from 'react';
-
+import logo from "../../public/logo.png"
 function Home () {
     return (
         <>
+        <img src='../../public/Logo.png'></img>
         <p>Home page</p>
         </>
     )
